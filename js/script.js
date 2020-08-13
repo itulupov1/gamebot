3f046a44923b3c349f5883979d09920452ba2313
itulupov1 <itulupov1@yandex.ru>
2020-08-14 00:00:07 +0700

    gamebot-easy

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
 'use strict';
-let i;
+
+// "Загадывание случайного числа от 1 до 100"
+
+let isNumber = function (n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+function setNumber() {
+	let number = parseInt(Math.random() * 100);
+	if (number === 0) {
+		number = 1;
+	}
+	function tryGuessing() {
+		let input = prompt('Угадай число от 1 до 100');
+		//setNumber();
+		if (input === null) {
+			return;
+		} else if (input > number) {
+			alert('Загаданное число меньше');
+			tryGuessing();
+		} else if (input < number) {
+			alert('Загаданное число больше');
+			tryGuessing();
+		} else if (!isNumber(input)) {
+			alert('Введи число!');
+			tryGuessing();
+		} else if (+input === number) {
+			alert('Ура, вы угадали число!');
+		}
+	}
+	tryGuessing(); 
+}
+setNumber();
+
