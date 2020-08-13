50bf28cbf8a5070ec0857ef9711fff30a8149193
itulupov1 <itulupov1@yandex.ru>
2020-08-14 00:29:01 +0700

    lesson06-complicated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// "Загадывание случайного числа от 1 до 100"
+// "Загадывание случайного числа от 1 до 100 - усложненное"
 
 let isNumber = function (n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
@@ -11,22 +11,35 @@ function setNumber() {
 	if (number === 0) {
 		number = 1;
 	}
+	let attempts = 10;
 	function tryGuessing() {
 		let input = prompt('Угадай число от 1 до 100');
-		//setNumber();
-		if (input === null) {
-			return;
-		} else if (input > number) {
-			alert('Загаданное число меньше');
-			tryGuessing();
-		} else if (input < number) {
-			alert('Загаданное число больше');
-			tryGuessing();
-		} else if (!isNumber(input)) {
-			alert('Введи число!');
-			tryGuessing();
-		} else if (+input === number) {
-			alert('Ура, вы угадали число!');
+		if (attempts > 0) {
+			if (input === null) {
+				alert('Пока...');
+				return;
+			} else if (input > number) {
+				attempts--;
+				alert('Загаданное число меньше, осталось попыток ' + attempts);
+				tryGuessing();
+			} else if (input < number) {
+				attempts--;
+				alert('Загаданное число больше, осталось попыток ' + attempts);
+				tryGuessing();
+			} else if (!isNumber(input)) {
+				alert('Введи число!');
+				tryGuessing();
+			} else if (+input === number) {
+				let congr = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+				if (congr === true) {
+					setNumber();
+				}
+			}
+		} else {
+			let lose = confirm('Попытки закончились, хотите сыграть еще?');
+			if (lose === true) {
+			setNumber();
+			}
 		}
 	}
 	tryGuessing(); 
