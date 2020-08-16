5412fdfa0f1e8d40dd2047dfcf6b3753b2f02585
itulupov1 <itulupov1@yandex.ru>
2020-08-16 14:08:05 +0700

    fix(js)

--- js/script.js
+++ js/script.js
@@ -2,31 +2,29 @@
 
 // "Загадывание случайного числа от 1 до 100"
 
-let isNumber = function (n) {
+const isNumber = function (n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 function setNumber() {
-	let number = parseInt(Math.random() * 100);
-	if (number === 0) {
-		number = 1;
-	}
+	const number = Math.round((Math.random() * 100)) + 1;
+
 	function tryGuessing() {
-		let input = prompt('Угадай число от 1 до 100');
+		const input = prompt('Угадай число от 1 до 100');
 		//setNumber();
 		if (input === null) {
-			return;
+			return false;
 		} else if (input > number) {
 			alert('Загаданное число меньше');
-			tryGuessing();
+			return tryGuessing();
 		} else if (input < number) {
 			alert('Загаданное число больше');
-			tryGuessing();
+			return tryGuessing();
 		} else if (!isNumber(input)) {
 			alert('Введи число!');
-			tryGuessing();
+			return tryGuessing();
 		} else if (+input === number) {
-			alert('Ура, вы угадали число!');
+			return alert('Ура, вы угадали число!');
 		}
 	}
 	tryGuessing(); 
